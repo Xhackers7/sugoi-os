99f271d7b25cb1028e29ce04bc36e6261acd63da
Xhackers7 <nishaajithkj@gmail.com>
2021-11-01 18:18:04 +0530

    Testing display

--- boot.asm
+++ boot.asm
@@ -1,59 +1,31 @@
 mov ah, 0x0e
-mov al, 'P'
-int 0x10
+mov al, 65
 
-mov ah, 0x0e
-mov al, 'o'
-int 0x10
+loop:
+    cmp al, 97
+    jge small
+    cmp al, 90
+    jle big
+    jmp mainloop
 
-mov ah, 0x0e
-mov al, 't'
-int 0x10
+big:
+    int 0x10
+    add al, 32
+    jmp mainloop
 
-mov ah, 0x0e
-mov al, 't'
-int 0x10
+small:
+    int 0x10
+    sub al, 32
 
-mov ah, 0x0e
-mov al, 'a'
-int 0x10
+mainloop:
+    inc al
+    cmp al, 90
+    je exit
+    cmp al, 122
+    je exit
+    jmp loop
 
-mov ah, 0x0e
-mov al, 'n'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'N'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'a'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'n'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'd'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'u'
-int 0x10
-
-mov ah, 0x0e
-mov al, ' '
-int 0x10
-
-mov ah, 0x0e
-mov al, 'O'
-int 0x10
-
-mov ah, 0x0e
-mov al, 'S'
-int 0x10
-
-jmp $
-times 510-($-$$) db 0
-db 0x55, 0xaa
+exit:
+    jmp $
+    times 510-($-$$) db 0
+    db 0x55, 0xaa
